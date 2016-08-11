2ed21326e2845b1e4242c635bbb9bbcca282c918
ALUNO <aluno@hc5mac15.local>
2016-08-10 22:27:02 -0300

    Fine application

--- Knowl/Knowl/ViewController.m
+++ Knowl/Knowl/ViewController.m
@@ -17,6 +17,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // Create the colors
+    UIColor *topColor = [UIColor colorWithRed:255.0/255.0 green:51.0/255.0 blue:255.0/255.0 alpha:1.0];
+    UIColor *bottomColor = [UIColor colorWithRed:56.0/255.0 green:56.0/255.0 blue:56.0/255.0 alpha:1.0];
+    
+    // Create the gradient
+    CAGradientLayer *theViewGradient = [CAGradientLayer layer];
+    theViewGradient.colors = [NSArray arrayWithObjects: (id)topColor.CGColor, (id)bottomColor.CGColor, nil];
+    theViewGradient.frame = self.view.bounds;
+    
+    //Add gradient to view
+    [self.view.layer insertSublayer:theViewGradient atIndex:0];
+    
 }
 
 - (void)didReceiveMemoryWarning {
